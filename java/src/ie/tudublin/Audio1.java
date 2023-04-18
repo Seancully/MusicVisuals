8f958f31b6ea45ae7c6d00777ab540ba8c5510d6
John Hinch <johnhnch864@gmail.com>
2023-04-18 11:45:44 +0100

    18/04/2023

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -16,7 +16,7 @@ public class Audio1 extends PApplet {
 	public Timer timer;
 
 	int mode = 0;
-	float seconds=0;
+	float seconds = 0;
 
 	float y = 0;
 	float smoothedY = 0;
@@ -34,29 +34,24 @@ public class Audio1 extends PApplet {
 				ap.play();
 			}
 		}
-		if (keyCode == '7')
-		{
-			seconds=0;
+		if (keyCode == '7') {
+			seconds = 0;
 		}
 	}
 
 	public void settings() {
-		size(1024, 1024, P3D);
-		// fullScreen(P3D, SPAN);
+		// size(1024, 1024, P3D);
+		fullScreen(P3D, SPAN);
 	}
 
 	public void setup() {
 		minim = new Minim(this);
-		// Uncomment this to use the microphone
-		// ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-		// ab = ai.mix;
 
 		timer = new Timer(this);
-		// And comment the next two lines out
+		// Load Music file
 		ap = minim.loadFile("Eyelar.mp3", 1024);
 		ap.play();
 		ab = ap.mix;
-		// colorMode(HSB);
 
 		y = height / 2;
 		smoothedY = y;
@@ -81,8 +76,9 @@ public class Audio1 extends PApplet {
 	}
 
 	public void draw() {
-		// background(0);
+		// timer.update();
 		float halfH = height / 2;
+		float halfW = width / 2;
 		float average = 0;
 		float sum = 0;
 		off += 1;
@@ -98,10 +94,9 @@ public class Audio1 extends PApplet {
 		float cx = width / 2;
 		float cy = height / 2;
 
-		//int seconds = 0;
-		seconds = second();
+		seconds = millis() / 1000.0f;
 		System.out.println(seconds);
-		if (seconds >=2 && seconds <6) {
+		if (seconds >= 2 && seconds < 6) {
 
 			background(0);
 			strokeWeight(1);
@@ -112,11 +107,10 @@ public class Audio1 extends PApplet {
 			fill(0, 0, 255);
 			textSize(200);
 			text("E", width / 2 - 350, height / 2 + 80);
-			
 
 		}
 
-		else if (seconds >= 6 && seconds<9) {
+		else if (seconds >= 6 && seconds < 9) {
 
 			background(0);
 			strokeWeight(1);
@@ -127,11 +121,10 @@ public class Audio1 extends PApplet {
 			fill(5, 92, 242);
 			textSize(200);
 			text("EY", width / 2 - 350, height / 2 + 80);
-			
 
 		}
 
-		else if (seconds >= 9 && seconds< 12) {
+		else if (seconds >= 9 && seconds < 12) {
 
 			background(0);
 			strokeWeight(1);
@@ -142,11 +135,10 @@ public class Audio1 extends PApplet {
 			fill(12, 71, 173);
 			textSize(200);
 			text("EYE", width / 2 - 350, height / 2 + 80);
-			
 
 		}
 
-		else if (seconds == 12 && seconds<15) {
+		else if (seconds >= 12 && seconds < 15) {
 
 			background(0);
 			strokeWeight(1);
@@ -159,7 +151,7 @@ public class Audio1 extends PApplet {
 
 		}
 
-		else if (seconds >=15&& seconds<18) {
+		else if (seconds >= 15 && seconds < 18) {
 
 			background(0);
 			strokeWeight(1);
@@ -172,7 +164,7 @@ public class Audio1 extends PApplet {
 
 		}
 
-		else if (seconds >= 18&& seconds<20) {
+		else if (seconds >= 18 && seconds < 20) {
 
 			background(0);
 			strokeWeight(1);
@@ -183,30 +175,20 @@ public class Audio1 extends PApplet {
 			textSize(200);
 			text("EYELAR", width / 2 - 350, height / 2 + 80);
 
-		}
-		else if (seconds >= 20&& seconds<25) {
+		} else if (seconds >= 20 && seconds < 25) {
 
 			background(0);
 			for (int i = 0; i < ab.size(); i++) {
-				// float c = map(ab.get(i), -1, 1, 0, 255);
 				float c = map(i, 0, ab.size(), 0, 255);
-				stroke(c, 255, 255);
+				stroke(c, 126, 127);
 				float f = ab.get(i) * halfH;
-				line(i, halfH + f, i, halfH - f);
+				line(i, halfH + f, width, halfH);
+				line(i, halfH + f, width, halfH);
 			}
-		} else if (seconds >= 25&& seconds<35) {
-			background(0);
-			// colorMode(HSB);
-			for (int i = 0; i < ab.size(); i++) {
-				float c = map(ab.get(i), -1, 1, 0, 255);
-				// float c = map(i, 0, ab.size(), 0, 255);
-				stroke(c, 255, 255);
-				float f = ab.get(i) * halfH;
-				line(i, halfH + f, halfH - f, i);
-			}
-		} 
+		} else if (seconds >= 25 && seconds < 35) {
+		}
 
-		 else if (seconds >= 35&& seconds<45) {
+		else if (seconds >= 35 && seconds < 45) {
 			background(0);
 			strokeWeight(2);
 			noFill();
@@ -238,33 +220,14 @@ public class Audio1 extends PApplet {
 
 		}
 
-		
-		 else if (seconds >=45 && seconds<48) {
+		else if (seconds >= 45 && seconds < 48) {
 			background(0);
-			for (int i = 0; i < ab.size(); i++) {
-
-				float color = map(i, 0, ab.size(), 0, 255);
-				stroke(color, 255, 255);
-				float f = ab.get(i) * halfH;
-				line(i, 0 + f, i, 0 - f);
-				line(0 + f, i, 0 - f, i);
-				line(height + f, i, height - f, i);
-				line(i, 1000 + f, i, 1000 - f);
-
-			}
-			strokeWeight(2);
-			noFill();
-			float radius = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
-			float l = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-			stroke(l, 255, 255);
-			circle(cx, cy, radius);
 		} else if (seconds >= 48) {
 			// here is where the main eye is made too
 
 			colorMode(RGB);
 			background(0, 255, 0);
 
-			float halfW = width / 2;
 			halfH = height / 2;
 
 			float total = 0;
@@ -306,27 +269,23 @@ public class Audio1 extends PApplet {
 
 			// create the iris
 			strokeWeight(10);
-			// float colors = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-			for (int i = 0; i < ab.size(); i++) {
-				// the iris
-				float radius = map(smoothedAmplitude, 0, 0.6f, width / 4, 500);
-				fill(255, 255, 255);
-				circle(halfW, halfH, radius);
+			// the iris
+			float radius = map(smoothedAmplitude, 0, 0.6f, width / 4, 500);
+			fill(255, 255, 255);
+			circle(halfW, halfH, radius);
 
-				// draw the pupil for the eye
-				float radius2 = map(smoothedAmplitude * 1.5f, 0, 0.7f, width / 20, 500);
-				fill(0);
-				circle(halfW, halfH, radius2 + 125);
+			// draw the pupil for the eye
+			float radius2 = map(smoothedAmplitude * 1.5f, 0, 0.7f, width / 20, 500);
+			fill(0);
+			circle(halfW, halfH, radius2 + 125);
 
-				fill(0, 0, 255);
-				circle(halfW, halfH, smoothedAmplitude);
-			}
+			fill(0, 0, 255);
+			circle(halfW, halfH, smoothedAmplitude);
 			// drawing 3 random stars
 			strokeWeight(2);
 			for (int i = 0; i < 6; i++) {
 				fill(0);
 				stroke(0);
-				// circle(random(width), random(height), random(width));
 				star(random(width), random(height), 10, 19, 5);
 				delay(10);
 			}
